52f3b973df59c4c4428e721abb6fc0f1b78ccc1d
Ruben Sostenes <47117456+rubensostenes@users.noreply.github.com>
2023-05-18 16:56:50 -0300

    Adição de novos arquivos

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -9,7 +9,7 @@ self.addEventListener("message", (event) => {
 });
 
 workbox.routing.registerRoute(
-  new RegExp('/*'),
+  new RegExp('index.html'),
   new workbox.strategies.StaleWhileRevalidate({
     cacheName: CACHE
   })
